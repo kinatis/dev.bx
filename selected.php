7026555f0c49c93c8a057eb1d44e37b225815e2e
Krupin Mikhail <krupin732@yandex.ru>
2021-11-07 01:21:24 +0200

    Страницы сайта

---
+++ selected.php
@@ -0,0 +1,13 @@
+<?php
+
+require "lib/template-function.php";
+require "resources/movies.php";
+require "config/config.php";
+
+$templateContent = "";
+
+
+
+
+
+renderLayout($templateContent, $genres, $side_bar_menu, 'selected');
