88aad2a161b4a67fcfa2f3ff7ac61d77c3e28798
Krupin Mikhail <krupin732@yandex.ru>
2021-11-08 15:58:06 +0200

    Перемещение дз по вёрсте в отдельную директорию

--- config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$side_bar_menu = [
-    'index' => "Главная",
-    'selected' => "Избранное"
-];
--- resources/pages/menu-item.php
+++
@@ -1,4 +0,0 @@
-<?php /** @var string $menu */ ?>
-<li class="menu-item ">
-    <a href="#"><?=$menu  ?></a>
-</li>
